f96add0935285ef761f41ac4c4b9f5ae2a1d52ec
Ben Drucker <bvdrucker@gmail.com>
2015-10-12 22:41:19 -0700

    Ensure that wrapping only happens once with multi chunk streams

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ var prefix = fs.readFileSync(resolve(__dirname, 'prefix.html'))
 var suffix = fs.readFileSync(resolve(__dirname, 'suffix.html'))
 
 module.exports = function htmlify () {
+  var first = true
   return through(function wrap (chunk, enc, callback) {
-    this.push(prefix)
+    if (first) this.push(prefix)
+    first = false
     this.push(chunk)
+    callback()
+  }, function (callback) {
     this.push(suffix)
     callback()
   })
